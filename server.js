4b8a580cd83c5981cb30d59fae87aa3272bc06a4
Duc Thai <duc111c@gmail.com>
2020-04-22 14:09:04 +0700

    Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (res, req) => {
+  console.log('API running...');
+  req.send('API running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
